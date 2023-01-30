9b1616485f23f4b2d8cfb837e484fccdf92c019a
Jonathan Bobrow <jbobrow@gmail.com>
2023-01-30 13:58:54 -0500

    Inform the user that a period has been logged.
    Also, only allow a period to be logged if it has been more than 2 weeks

--- Period-Logger.js
+++ Period-Logger.js
@@ -8,12 +8,41 @@ fileManager.downloadFileFromiCloud(file);
 
 if(fileManager.fileExists(file)) {
   console.log("File found!");
-  var dateToAdd = new Date();
-  var dateString = dateToAdd.toLocaleDateString();
-  var data = fileManager.readString(file);
-  data = data + ",\n" + dateString;
-  fileManager.writeString(file, data);
-  console.log(dateString + " added ✅");
+  
+  // determine if it makes sense to add the date (i.e. more than 14 days away)
+  var prevPeriodDate = getLastPeriodDate(file);
+  var now = new Date().getTime();
+  var diff = prevPeriodDate - now;
+  var daysSince = Math.abs(Math.floor(diff / (1000 * 60 * 60 * 24)));
+
+  
+  if( daysSince > 14 ) {
+
+    var dateToAdd = new Date();
+    var dateString = dateToAdd.toLocaleDateString();
+    var data = fileManager.readString(file);
+    data = data + ",\n" + dateString;
+    fileManager.writeString(file, data);
+    console.log(dateString + " added ✅");
+    
+    // show the user
+    let alert = new Alert();
+    alert.title = "✅ Period Logged ✅";
+    alert.message = "Added " + dateString + " to the period log.";
+    alert.addAction("Thanks!");
+    alert.present();
+  }
+  else {
+    
+    // show the user
+    let alert = new Alert();
+    alert.title = "⚠️No Period Logged⚠️";
+    alert.message = "It's only been " + daysSince + " day(s) since the last logged period. No period Logged";
+    alert.addCancelAction("Thanks... 😬");
+    alert.present();
+
+  }
+  
 }
 else {
   var dateToAdd = new Date();
@@ -21,7 +50,29 @@ else {
   fileManager.writeString(file, dateString);
   console.log("No File found, so we created one!");
   console.log(dateString + " added ✅");
+  
+  // show the user
+  let alert = new Alert();
+  alert.title = "⚠️Period Log Created👍";
+  alert.message = "No File found, so we created one!" + dateString + " added.";
+  alert.addAction("Thanks!");
+  alert.present();
 }
 
 
 Script.complete();
+
+/*
+ * Get the last recorded Period date from the file
+ */
+function getLastPeriodDate(file) {
+  
+  var data = fileManager.readString(file);
+  var dates = data.split(',');
+  console.log(dates.length);
+  
+  var date = new Date(dates[dates.length-1]).getTime();
+  
+  return date;
+}
+
